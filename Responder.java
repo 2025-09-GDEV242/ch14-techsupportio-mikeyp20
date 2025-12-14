b3504835e8f45dbda7131e69a1a21edda223fc4f
mikeyp20 <mikeypatterson03@aol.com>
2025-12-13 21:50:04 -0500

    Finished

--- Responder.java
+++ Responder.java
@@ -14,8 +14,8 @@ import java.util.*;
  * in the HashMap, the corresponding response is returned. If none of the input
  * words is recognized, one of the default responses is randomly chosen.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Michael Patterson 
+ * @version 2025
  */
 public class Responder
 {
@@ -67,52 +67,40 @@ public class Responder
      */
     private void fillResponseMap()
     {
-        responseMap.put("crash", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("crashes", 
-                        "Well, it never crashes on our system. It must have something\n" +
-                        "to do with your system. Tell me more about your configuration.");
-        responseMap.put("slow", 
-                        "I think this has to do with your hardware. Upgrading your processor\n" +
-                        "should solve all performance problems. Have you got a problem with\n" +
-                        "our software?");
-        responseMap.put("performance", 
-                        "Performance was quite adequate in all our tests. Are you running\n" +
-                        "any other processes in the background?");
-        responseMap.put("bug", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("buggy", 
-                        "Well, you know, all software has some bugs. But our software engineers\n" +
-                        "are working very hard to fix them. Can you describe the problem a bit\n" +
-                        "further?");
-        responseMap.put("windows", 
-                        "This is a known bug to do with the Windows operating system. Please\n" +
-                        "report it to Microsoft. There is nothing we can do about this.");
-        responseMap.put("macintosh", 
-                        "This is a known bug to do with the Mac operating system. Please\n" +
-                        "report it to Apple. There is nothing we can do about this.");
-        responseMap.put("expensive", 
-                        "The cost of our product is quite competitive. Have you looked around\n" +
-                        "and really compared our features?");
-        responseMap.put("installation", 
-                        "The installation is really quite straight forward. We have tons of\n" +
-                        "wizards that do all the work for you. Have you read the installation\n" +
-                        "instructions?");
-        responseMap.put("memory", 
-                        "If you read the system requirements carefully, you will see that the\n" +
-                        "specified memory requirements are 1.5 giga byte. You really should\n" +
-                        "upgrade your memory. Anything else you want to know?");
-        responseMap.put("linux", 
-                        "We take Linux support very seriously. But there are some problems.\n" +
-                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
-                        "more precise?");
-        responseMap.put("bluej", 
-                        "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
-                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
-                        "do about it, I'm afraid.");
+        responseMap.clear();
+        
+        Charset charset = Charset.forName("US-ASCII");
+        Path path = Paths.get("responses.txt");
+        
+        try(BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            String line = reader.readLine();
+            
+            while(line != null) {
+                if(line.trim().isEmpty()) {
+                    line = reader.readLine();
+                    continue;
+                }
+                String[] keys = line.split(", ");
+                StringBuilder response = new StringBuilder(); 
+                line = reader.readLine(); 
+                
+                while(line != null && !line.trim().isEmpty()) {
+                    if(response.length() > 0) {
+                        response.append(" ");
+                    }
+                    response.append(line);
+                    line = reader.readLine();
+                }
+                for(String key : keys) {
+                    responseMap.put(key.trim(),response.toString());
+                }
+                if(line!=null) {
+                    line = reader.readLine();
+                }
+            }
+        }catch(Exception e ) {
+            System.out.println("Error reading responses.txt:" + e.getMessage());
+        }
     }
 
     /**
@@ -121,29 +109,39 @@ public class Responder
      */
     private void fillDefaultResponses()
     {
-        Charset charset = Charset.forName("US-ASCII");
+      Charset charset = Charset.forName("US-ASCII");
         Path path = Paths.get(FILE_OF_DEFAULT_RESPONSES);
         try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
-            String response = reader.readLine();
-            while(response != null) {
-                defaultResponses.add(response);
-                response = reader.readLine();
+        StringBuilder currentResponse = new StringBuilder();
+        String line = reader.readLine();
+        while(line != null) {
+            if(line.trim().isEmpty()) {
+                if(currentResponse.length() > 0) {
+                    defaultResponses.add(currentResponse.toString());
+                    currentResponse.setLength(0);
+                }
             }
+            else {
+                if(currentResponse.length() > 0) {
+                    currentResponse.append(" ");
+                }
+                currentResponse.append(line);
+            }
+            line = reader.readLine();
         }
-        catch(FileNotFoundException e) {
-            System.err.println("Unable to open " + FILE_OF_DEFAULT_RESPONSES);
+        if(currentResponse.length() > 0) {
+            defaultResponses.add(currentResponse.toString());
         }
-        catch(IOException e) {
-            System.err.println("A problem was encountered reading " +
-                               FILE_OF_DEFAULT_RESPONSES);
+        }
+        catch(Exception e) {
+            System.out.println("Error reading default.txt: " + e.getMessage());
         }
         // Make sure we have at least one response.
         if(defaultResponses.size() == 0) {
             defaultResponses.add("Could you elaborate on that?");
         }
-    }
-
-    /**
+}
+/**
      * Randomly select and return one of the default responses.
      * @return     A random default response
      */
